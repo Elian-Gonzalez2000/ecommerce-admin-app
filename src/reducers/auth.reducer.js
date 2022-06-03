a194a0094e32690f1acf7bebeae0026ab515c0dc
Elian <eliancarlogm@gmail.com>
2022-06-03 18:15:31 -0430

    feat(src/reducers): configure redux reducers folders and files

---
+++ src/reducers/auth.reducer.js
@@ -0,0 +1,17 @@
+import { authContants } from "../actions/constants";
+
+const initialState = {
+   name: "Riz",
+};
+
+export default (state = initialState, action) => {
+   switch (action.type) {
+      case authConstants.LOGIN_REQUEST:
+         state = {
+            ...state,
+            ...action,
+         };
+         break;
+   }
+   return state;
+};
