0eee2c9bd9513feed343e285bc2b2e0be9633abc
Elian <eliancarlogm@gmail.com>
2022-06-14 13:17:38 -0430

    feat(containers/Category): Make the renderCategories function that have the logic for render all categories of the list

--- src/containers/Category/index.jsx
+++ src/containers/Category/index.jsx
@@ -1,15 +1,31 @@
 import React, { useEffect } from "react";
 import Layout from "../../components/Layout";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllCategory } from "../../actions";
 
 const Category = () => {
+   const categories = useSelector((state) => state.categories);
    const dispatch = useDispatch();
 
    useEffect(() => {
       dispatch(getAllCategory());
    }, []);
+
+   const renderCategories = (categoryList) => {
+      let myCategories = [];
+      for (let category of categoryList) {
+         myCategories.push(
+            <li key={category.name}>
+               {category.name}
+               {category.children.length > 0 ? (
+                  <ul>{renderCategories(category.children)}</ul>
+               ) : null}
+            </li>
+         );
+      }
+      return categories;
+   };
 
    return (
       <Layout sidebar>
@@ -28,7 +44,7 @@ const Category = () => {
                </Col>
             </Row>
             <Row>
-               <Col md={12}></Col>
+               <Col md={12}>{renderCategories(categories)}</Col>
             </Row>
          </Container>
       </Layout>
