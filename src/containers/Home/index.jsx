2576fb8279d944ad44c12dd978b81022cf5192b5
Elian <eliancarlogm@gmail.com>
2022-06-13 21:36:51 -0430

    refactor(containers/Home): delete sidebar and it was added in Layout compoenent

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import { Row, Col, Container } from "react-bootstrap";
 import "./style.css";
 
 const Home = (props) => {
    return (
-      <Layout>
-         <Container>
-            <Row>
-               <Col md={2} className="sidebar">
-                  Sidebar
-               </Col>
-               <Col md={10} style={{ marginLeft: "auto" }}>
-                  Container
-               </Col>
-            </Row>
-         </Container>
-         {/* <h1>Welcome Home</h1> */}
+      <Layout sidebar>
+         <h1>Welcome Home</h1>
       </Layout>
    );
 };
