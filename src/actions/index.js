9befb44a3433391385e8ad7bca003771adc5596a
Elian <eliancarlogm@gmail.com>
2022-06-14 12:32:46 -0430

    feat: dispatch getAllCategory in Category when the component did mount

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,3 @@
 export * from "./auth.action";
 export * from "./user.actions";
+export * from "./category.action";
--- src/containers/Category/index.jsx
+++ src/containers/Category/index.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../../components/Layout";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { getAllCategory } from "../../actions";
 
 const Category = () => {
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      dispatch(getAllCategory());
+   }, []);
+
    return (
       <Layout sidebar>
-         <h1>Categories</h1>
+         <Container>
+            <Row>
+               <Col md={12}>
+                  <div
+                     style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                     }}
+                  >
+                     <h3>Category</h3>
+                     <Button>Add</Button>
+                  </div>
+               </Col>
+            </Row>
+            <Row>
+               <Col md={12}></Col>
+            </Row>
+         </Container>
       </Layout>
    );
 };
