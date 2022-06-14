fad0e550b99f8b49af28ee568e35335f90667e11
Elian <eliancarlogm@gmail.com>
2022-06-14 11:33:57 -0430

    feat(reducers/index.js): Include news reducers like category, order, product

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import authReducer from "./auth.reducer";
 import userReducer from "./user.reducer";
+import categoryReducer from "./category.reducer";
+import productReducer from "./product.reducer";
+import orderReducer from "./order.reducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
    auth: authReducer,
    user: userReducer,
+   category: categoryReducer,
+   order: orderReducer,
+   product: productReducer,
 });
 
 export default rootReducer;
