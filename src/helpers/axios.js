419d5a94addfd856ba69fafda1b5239b00bf434b
Elian <eliancarlogm@gmail.com>
2022-06-13 19:52:23 -0430

    feat(helpers/axios): add Authorization header with the token in the helpers

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { api } from "../urlConfig";
 
+const token = localStorage.getItem("token");
+
 const axiosInstance = axios.create({
    baseURL: api,
-   /* headers: {
-      "Authorization": "",
-   } */
+   headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+   },
 });
 
 export default axiosInstance;
