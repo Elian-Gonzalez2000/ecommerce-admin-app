c7a8e43bb149dbe37e7fc7998ce5c1e39eb06ecd
Elian <eliancarlogm@gmail.com>
2022-06-13 21:33:45 -0430

    feat(components/Layout): Improve layout component for render the sidebar or not depending of a prop sidebar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,39 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import Header from "../Header";
 
 const Layout = (props) => {
    return (
       <>
          <Header />
-         <Container>{props.children}</Container>
+         {props.sidebar ? (
+            <Container>
+               <Row>
+                  <Col md={2} className="sidebar">
+                     <ul>
+                        <li>
+                           {" "}
+                           <NavLink to={"/"}>Home</NavLink>{" "}
+                        </li>
+                        <li>
+                           {" "}
+                           <NavLink to={"/products"}>Products</NavLink>{" "}
+                        </li>
+                        <li>
+                           {" "}
+                           <NavLink to={"/orders"}>Orders</NavLink>{" "}
+                        </li>
+                     </ul>
+                  </Col>
+                  <Col md={10} style={{ marginLeft: "auto" }}>
+                     {props.children}
+                  </Col>
+               </Row>
+            </Container>
+         ) : (
+            props.children
+         )}
       </>
    );
 };
