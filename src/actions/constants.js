a7ea90f782b88cb784aa22cc3cdd9acf70c4c738
Elian <eliancarlogm@gmail.com>
2022-06-13 19:56:08 -0430

    feat(actions/constants): Add constants LOGOUT_R EQUEST, LOGOUT_SUCCESS, LOGOUT_FAILURE into authConstants

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -3,6 +3,8 @@ export const authConstants = {
    LOGIN_FAILURE: "LOGIN_FAILURE",
    LOGIN_SUCCESS: "LOGIN_SUCCESS",
    LOGOUT_REQUEST: "LOGOUT_REQUEST",
+   LOGOUT_SUCCESS: "LOGOUT_SUCCESS",
+   LOGOUT_FAILURE: "LOGOUT_FAILURE",
 };
 
 export const userConstants = {
