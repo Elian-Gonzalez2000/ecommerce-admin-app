7e1e8997844778249eed2bf850703344c2c80ac0
Elian <eliancarlogm@gmail.com>
2022-06-14 19:33:02 -0430

    feat(containers/Category): Add modal with bootstrap to the form of the button Add, this inputs send the data for create category

--- src/containers/Category/index.jsx
+++ src/containers/Category/index.jsx
@@ -1,16 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCategory } from "../../actions";
+import Input from "../../components/UI/Input";
 
 const Category = () => {
-   const categories = useSelector((state) => state.categories);
+   const [categoryName, setCategoryName] = useState("");
+   const [parentCategoryId, setParentCategoryId] = useState("");
+   const [categoryImage, setCategoryImage] = useState("");
+   const [show, setShow] = useState(false);
+   const category = useSelector((state) => state.category);
    const dispatch = useDispatch();
 
    useEffect(() => {
       dispatch(getAllCategory());
    }, []);
+
+   const handleClose = () => {
+      const form = new FormData();
+      form.append("name", categoryName);
+      form.append("parentId", parentCategoryId);
+      form.append("categoryImage", categoryImage);
+
+      // const cat = {
+      //    categoryName,
+      //    parentCategoryId,
+      //    categoryImage,
+      // };
+      // console.log(cat);
+      setShow(false);
+   };
+   const handleShow = () => setShow(true);
 
    const renderCategories = (categoryList) => {
       let myCategories = [];
@@ -27,6 +48,20 @@ const Category = () => {
       return categories;
    };
 
+   const createCategoryList = (categories, options = []) => {
+      for (let category of categories) {
+         options.push({ value: category._id, name: category.name });
+         if (category.children.length > 0) {
+            createCategoryList(category.children, options);
+         }
+      }
+      return options;
+   };
+
+   const handleCategoryImage = () => {
+      setCategoryImage(e.target.files[0]);
+   };
+
    return (
       <Layout sidebar>
          <Container>
@@ -39,14 +74,53 @@ const Category = () => {
                      }}
                   >
                      <h3>Category</h3>
-                     <Button>Add</Button>
+                     <Button onClick={handleShow}>Add</Button>
                   </div>
                </Col>
             </Row>
             <Row>
-               <Col md={12}>{renderCategories(categories)}</Col>
+               <Col md={12}>{renderCategories(category.categories)}</Col>
             </Row>
          </Container>
+
+         <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+               <Modal.Title>Add category</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+               <Input
+                  value={categoryName}
+                  placeholder={"Category Name"}
+                  onChange={(e) => setCategoryName(e.target.value)}
+               />
+
+               <select
+                  className="form-control"
+                  value={parentCategoryId}
+                  onChange={(e) => setParentCategoryId(e.target.value)}
+               >
+                  <option>Select Category</option>
+                  {createCategoryList(category.categories).map((option) => (
+                     <option key={option.value} value={option.value}>
+                        {option.name}
+                     </option>
+                  ))}
+               </select>
+               <input
+                  type="file"
+                  name="categoryImage"
+                  onChange={handleCategoryImage}
+               />
+            </Modal.Body>
+            <Modal.Footer>
+               <Button variant="secondary" onClick={handleClose}>
+                  Close
+               </Button>
+               <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+               </Button>
+            </Modal.Footer>
+         </Modal>
       </Layout>
    );
 };
