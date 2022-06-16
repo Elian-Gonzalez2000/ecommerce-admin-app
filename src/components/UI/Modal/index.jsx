2d509d8b714d41c37feb058430ee207c4ca89d05
Elian <eliancarlogm@gmail.com>
2022-06-16 12:23:16 -0430

    feat(UI/Modal): Make a component modal to refactor the code of the Category and Product components

---
+++ src/components/UI/Modal/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+const Modal = (props) => {
+   return (
+      <Modal show={props.show} onHide={props.handleClose}>
+         <Modal.Header closeButton>
+            <Modal.Title>{props.modalTitle}</Modal.Title>
+         </Modal.Header>
+         <Modal.Body>{props.children}</Modal.Body>
+         <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+               Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+               Save Changes
+            </Button>
+         </Modal.Footer>
+      </Modal>
+   );
+};
+
+export default Modal;
