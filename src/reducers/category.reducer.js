81c4d86dbf6bda3e7034d3136b96434035103bc0
Elian <eliancarlogm@gmail.com>
2022-06-15 20:31:05 -0430

    feat(reducers/category.reducer): complete logic case ADD_NEW_CATEGORY_SUCCES, when create a new category in the admin dashboard, the function buildNewCategories search the parecnt of the category and it paste like a children

--- src/reducers/category.reducer.js
+++ src/reducers/category.reducer.js
@@ -6,6 +6,43 @@ const initialState = {
    error: null,
 };
 
+const buildNewCategories = (parentId, categories, category) => {
+   let myCategories = [];
+   for (let cat of categories) {
+      if (cat._id == parentId) {
+         myCategories.push({
+            ...cat,
+            children:
+               cat.children && cat.children.length > 0
+                  ? buildNewCategories(
+                       parentId,
+                       [
+                          ...car.children,
+                          {
+                             _id: category._id,
+                             name: category.name,
+                             slug: category.slug,
+                             parentId: category.parentId,
+                             children: category.children,
+                          },
+                       ],
+                       category
+                    )
+                  : [],
+         });
+      } else {
+         myCategories.push({
+            ...cat,
+            children:
+               cat.children && cat.children.length > 0
+                  ? buildNewCategories(parentId, car.children, category)
+                  : [],
+         });
+      }
+   }
+   return myCategories;
+};
+
 export default (state = initialState, action) => {
    switch (action.type) {
       case categoryConstants.GET_ALL_CATEGORIES_SUCCESS:
@@ -23,8 +60,16 @@ export default (state = initialState, action) => {
          break;
 
       case categoryConstants.ADD_NEW_CATEGORY_SUCCESS:
+         const category = action.payload.category;
+         const updateCategories = buildNewCategories(
+            category.parentId,
+            state.categories,
+            category
+         );
+
          state = {
             ...state,
+            categories: updateCategories,
             loading: false,
          };
          break;
