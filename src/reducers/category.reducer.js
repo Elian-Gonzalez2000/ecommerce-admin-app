915cf5bb5c0f4132b7539f8e19dd79401830e8b4
Elian <eliancarlogm@gmail.com>
2022-06-16 12:34:10 -0430

    refactor(reducers/category.reducer): when buildNewCategories recive a parentId parameter like undifined, the function returned a copy of the same categories but adding the new category created, this is to create mothers categories wihout any problem

--- src/reducers/category.reducer.js
+++ src/reducers/category.reducer.js
@@ -8,35 +8,46 @@ const initialState = {
 
 const buildNewCategories = (parentId, categories, category) => {
    let myCategories = [];
+
+   if (parentId == undefined) {
+      return [
+         ...categories,
+         {
+            _id: category._id,
+            name: category_name,
+            slug: category.slug,
+            children: [],
+         },
+      ];
+   }
+
    for (let cat of categories) {
       if (cat._id == parentId) {
          myCategories.push({
             ...cat,
-            children:
-               cat.children && cat.children.length > 0
-                  ? buildNewCategories(
-                       parentId,
-                       [
-                          ...car.children,
-                          {
-                             _id: category._id,
-                             name: category.name,
-                             slug: category.slug,
-                             parentId: category.parentId,
-                             children: category.children,
-                          },
-                       ],
-                       category
-                    )
-                  : [],
+            children: cat.children
+               ? buildNewCategories(
+                    parentId,
+                    [
+                       ...car.children,
+                       {
+                          _id: category._id,
+                          name: category.name,
+                          slug: category.slug,
+                          parentId: category.parentId,
+                          children: category.children,
+                       },
+                    ],
+                    category
+                 )
+               : [],
          });
       } else {
          myCategories.push({
             ...cat,
-            children:
-               cat.children && cat.children.length > 0
-                  ? buildNewCategories(parentId, car.children, category)
-                  : [],
+            children: cat.children
+               ? buildNewCategories(parentId, car.children, category)
+               : [],
          });
       }
    }
