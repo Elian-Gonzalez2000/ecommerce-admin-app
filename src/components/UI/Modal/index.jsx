ef4deca398bb0bf79a43701b5d094c57263fca13
Elian <eliancarlogm@gmail.com>
2022-06-18 22:46:03 -0430

    feat(UI/Modal): Add prop size, this defined the size of the modal, like lg, md, etc

--- src/components/UI/Modal/index.jsx
+++ src/components/UI/Modal/index.jsx
@@ -3,7 +3,7 @@ import { Modal, Button } from "react-bootstrap";
 
 const Modal = (props) => {
    return (
-      <Modal show={props.show} onHide={props.handleClose}>
+      <Modal size={props.size} show={props.show} onHide={props.handleClose}>
          <Modal.Header closeButton>
             <Modal.Title>{props.modalTitle}</Modal.Title>
          </Modal.Header>
