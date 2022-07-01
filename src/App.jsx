ae5d3f9720f0a11ac1654f512dc910a111c99677
Elian <eliancarlogm@gmail.com>
2022-07-01 12:22:58 -0430

    refactor(src/App): Put dispatch(getInitialState) into a conditional

--- src/App.jsx
+++ src/App.jsx
@@ -59,8 +59,10 @@ function App() {
       if (!auth.authenticate) {
          dispatch(isUserLoggedIn());
       }
-      dispatch(getInitialData());
-   }, []);
+      if (auth.authenticate) {
+         dispatch(getInitialData());
+      }
+   }, [auth.authenticate]);
    return (
       <div className="App">
          <Routes>
