06fe08e662a33b75dbc4d0821b0c0f1b71e3c7f9
Elian <eliancarlogm@gmail.com>
2022-07-01 13:48:05 -0430

    test(reducers/auth.reducer): test the user data and delete some consoles.log

--- src/reducers/auth.reducer.js
+++ src/reducers/auth.reducer.js
@@ -16,7 +16,7 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-   console.log(action);
+   //console.log(action);
    switch (action.type) {
       case authConstants.LOGIN_REQUEST:
          state = {
