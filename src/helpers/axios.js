e00fb4c3ec25b4baaa8b71d4f2f5f82962e8e466
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-13 23:53:12 -0400

    feat(helpers/axios): improving adding interceptors for the response and request to verific the token when is expire, then call the action to logout the user

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { api } from "../urlConfig";
+import store from "../store";
+import { authConstants } from "../actions/constants";
 
 const token = localStorage.getItem("token");
 
@@ -11,5 +13,28 @@ const axiosInstance = axios.create({
       Authorization: token ? `Bearer ${token}` : "",
    },
 });
+
+axiosInstance.interceptors.request.use((req) => {
+   const { auth } = store.getState();
+   if (auth.token) {
+      req.headers.Authorization = `Bearer ${auth.token}`;
+   }
+   return req;
+});
+
+axiosInstance.interceptors.response.use(
+   (res) => {
+      return res;
+   },
+   (error) => {
+      const { status } = error.response;
+      console.log(error.response);
+      if (status === 500 || status === 400) {
+         localStorage.clear();
+         store.dispatch({ type: authConstants.LOGOUT_SUCCESS });
+      }
+      return Promise.reject(error);
+   }
+);
 
 export default axiosInstance;
