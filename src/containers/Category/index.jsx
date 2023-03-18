c0ed8a890d81671241a073064d431700ab6e42d1
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-18 12:59:03 -0400

    feat(containers/Category): add property type in createCategoriesList fucntion

--- src/containers/Category/index.jsx
+++ src/containers/Category/index.jsx
@@ -84,6 +84,7 @@ const Category = () => {
             value: category._id,
             name: category.name,
             parentId: category.parentId,
+            type: category.type,
          });
          if (category.children.length > 0) {
             createCategoryList(category.children, options);
