adf66d91d8076b52873b283d2c9b84e5b434c218
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-18 21:26:43 -0400

    feat(containers/NewPage): Add and complete some functions like submitPageForm, onCategoryChange and render some data about banners and products. Some changes in the properties of the components

--- src/containers/NewPage/index.jsx
+++ src/containers/NewPage/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Layout from "../../components/Layout";
 import Input from "../../components/UI/Input";
 import Modal from "../../components/UI/Modal";
 import linearCategories from "../../helpers/linearCategories";
+import { createPage } from "../../actions";
 
 function NewPage() {
    const [createModal, setCreateModal] = useState(false);
@@ -13,19 +14,56 @@ function NewPage() {
    const [categories, setCategories] = useState([]);
    const [categoryId, setCategoryId] = useState("");
    const [description, setDescription] = useState("");
+   const [type, setType] = useState("");
    const [banners, setBanners] = useState([]);
    const [products, setProducts] = useState([]);
+   const dispatch = useDispatch();
 
    useEffect(() => {
       setCategories(linearCategories(category.categories));
-   }, []);
+   }, [category]);
+
+   const onCategoryChange = (e) => {
+      const category = categories.find(
+         (category) => category._id == e.target.value
+      );
+      setCategoryId(e.target.value);
+      setType(category.type);
+   };
 
    const handleBannersImage = (e) => {
       console.log(e);
+      setBanners([...banners, e.target.files[0]]);
    };
 
    const handleProductsImage = (e) => {
       console.log(e);
+      setProducts([...products, e.target.files[0]]);
+   };
+
+   const submitPageForm = (e) => {
+      //e.target.preventDefault();
+
+      if (title === "") {
+         alert("Title is required");
+         setCreateModal(false);
+         return;
+      }
+
+      const form = new FormData();
+      form.append("title", title);
+      form.append("description", description);
+      form.append("category", categoryId);
+      form.append("type", type);
+      banners.forEach((banner, index) => {
+         form.append("banners", banner);
+      });
+      products.forEach((product, index) => {
+         form.append("products", product);
+      });
+
+      dispatch(createPage(form));
+      setCreateModal(false);
    };
 
    const renderCreatePageModal = () => {
@@ -33,7 +71,7 @@ function NewPage() {
          <Modal
             show={createModal}
             modalTitle={"Create New Page"}
-            handleClose={() => setCreateModal(false)}
+            handleClose={submitPageForm}
          >
             <Container>
                <Row>
@@ -75,6 +113,13 @@ function NewPage() {
                   </Col>
                </Row>
                <Row>
+                  {banners.length > 0
+                     ? banners.map((banner, index) => (
+                          <Row key={`${index}-${banner.name}`}>
+                             <Col>{banner.name}</Col>
+                          </Row>
+                       ))
+                     : null}
                   <Col>
                      <input
                         type="file"
@@ -85,6 +130,13 @@ function NewPage() {
                   </Col>
                </Row>
                <Row>
+                  {products.length > 0
+                     ? products.map((product, index) => (
+                          <Row key={`${index}-${product.name}`}>
+                             <Col>{product.name}</Col>
+                          </Row>
+                       ))
+                     : null}
                   <Col>
                      <input
                         type="file"
