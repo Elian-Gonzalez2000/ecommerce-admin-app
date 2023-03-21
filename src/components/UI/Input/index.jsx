b3ca74674798481dd7ce26d71501ca6b201bc49f
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-20 23:18:33 -0400

    feat(components/UI/Input): Improve the component adding a conditional render depending of the input type

--- src/components/UI/Input/index.jsx
+++ src/components/UI/Input/index.jsx
@@ -7,19 +7,51 @@ import { Form } from "react-bootstrap";
 */
 const Input = (props) => {
    const { label, type, placeholder, value, onChange, errorMessage } = props;
-   return (
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-         {label && <Form.Label>{label}</Form.Label>}
-         <Form.Control
-            type={type}
-            placeholder={placeholder}
-            value={value}
-            onChange={onChange}
-            className={props.className}
-         />
-         <Form.Text className="text-muted">{errorMessage}</Form.Text>
-      </Form.Group>
-   );
+
+   let input = null;
+   switch (type) {
+      case "select":
+         input = (
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+               {label && <Form.Label>{label}</Form.Label>}
+               <select
+                  className="form-control form-control-sm"
+                  value={value}
+                  onChange={onChange}
+               >
+                  <option value="">{placeholder}</option>
+                  {props.options.length > 0
+                     ? props.options.map((option, index) => (
+                          <option
+                             key={`${option.name}-${index}`}
+                             value={option._id}
+                          >
+                             {option.name}
+                          </option>
+                       ))
+                     : null}
+               </select>
+            </Form.Group>
+         );
+         break;
+      case "text":
+      default:
+         input = (
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+               {label && <Form.Label>{label}</Form.Label>}
+               <Form.Control
+                  type={type}
+                  placeholder={placeholder}
+                  value={value}
+                  onChange={onChange}
+                  className={props.className}
+               />
+               <Form.Text className="text-muted">{errorMessage}</Form.Text>
+            </Form.Group>
+         );
+         break;
+   }
+   return input;
 };
 
 export default Input;
