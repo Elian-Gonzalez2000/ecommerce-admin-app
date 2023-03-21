e60c5de2574ba772b2be86ea7204cb44a0dd5187
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-20 23:19:16 -0400

    delete a property

--- src/helpers/linearCategories.js
+++ src/helpers/linearCategories.js
@@ -2,7 +2,6 @@ const linearCategories = (categories, options = []) => {
    for (let category of categories) {
       options.push({
          _id: category._id,
-         value: category._id,
          name: category.name,
          parentId: category.parentId,
          type: category.type,
