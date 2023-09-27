abcfce1dbb1e83251f2d20e20382c24e13074882
Elian Gonzalez <eliancarlogm@gmail.com>
2023-09-26 23:14:04 -0400

    feat(src/reducers/order.reducer.js): make the order reducer

---
+++ src/reducers/order.reducer.js
@@ -0,0 +1,17 @@
+const { orderConstants } = require("../actions/constants");
+
+const initState = {
+   orders: [],
+};
+
+export default (state = initState, action) => {
+   switch (action.type) {
+      case orderConstants.GET_CUSTOMER_ORDER_SUCCESS:
+         state = {
+            ...state,
+            orders: action.payload.orders,
+         };
+         break;
+   }
+   return state;
+};
