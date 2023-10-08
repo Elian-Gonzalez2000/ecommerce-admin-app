6644d2278e0e556b3b50d5c6cff90968a69a29bc
Elian Gonzalez <eliancarlogm@gmail.com>
2023-10-08 18:52:41 -0400

    feat(src/containers/Orders/): improve the layout of the page and update some styles

--- src/containers/Orders/index.jsx
+++ src/containers/Orders/index.jsx
@@ -6,11 +6,6 @@ import Card from "../../components/UI/Card";
 import { randomUI } from "../../helpers/randomUI";
 import "./style.css";
 
-/**
- * @author
- * @function Orders
- **/
-
 const Orders = (props) => {
    const order = useSelector((state) => state.order);
    const [type, setType] = useState("");
@@ -37,27 +32,27 @@ const Orders = (props) => {
       <Layout sidebar>
          {order &&
             order.orders.map((orderItem) => (
-               <Card key={randomUI()} headerLeft={orderItem._id}>
+               <Card
+                  key={randomUI()}
+                  headerLeft={orderItem._id}
+                  style={{ margin: "2rem auto" }}
+               >
                   <div
                      style={{
-                        boxSizing: "border-box",
-                        padding: "100px",
-                        display: "flex",
-                        alignItems: "center",
+                        padding: "2rem 1rem",
                      }}
                   >
                      <div
                         style={{
                            display: "flex",
                            justifyContent: "space-between",
-                           padding: "50px 50px",
-                           alignItems: "center",
+                           paddingBottom: "50px",
                         }}
                      >
                         <div>
                            <div className="title">Items</div>
-                           {orderItem.items.map((item, index) => (
-                              <div className="value" key={index}>
+                           {orderItem.items.map((item) => (
+                              <div key={randomUI()} className="value">
                                  {item.productId.name}
                               </div>
                            ))}
@@ -82,28 +77,35 @@ const Orders = (props) => {
                            </span>
                         </div>
                      </div>
-                     <div className="orderTrack">
-                        {orderItem.orderStatus.map((status) => (
-                           <div
-                              className={`orderStatus ${
-                                 status.isCompleted ? "active" : ""
-                              }`}
-                           >
+                     <div
+                        style={{
+                           display: "flex",
+                           justifyContent: "space-evenly",
+                        }}
+                     >
+                        <div className="orderTrack">
+                           {orderItem.orderStatus.map((status) => (
                               <div
-                                 className={`point ${
+                                 key={randomUI()}
+                                 className={`orderStatus ${
                                     status.isCompleted ? "active" : ""
                                  }`}
-                              ></div>
-                              <div className="orderInfo">
-                                 <div className="status">{status.type}</div>
-                                 <div className="date">
-                                    {formatDate(status.date)}
+                              >
+                                 <div
+                                    className={`point ${
+                                       status.isCompleted ? "active" : ""
+                                    }`}
+                                 ></div>
+                                 <div className="orderInfo">
+                                    <div className="status">{status.type}</div>
+                                    <div className="date">
+                                       {formatDate(status.date)}
+                                    </div>
                                  </div>
                               </div>
-                           </div>
-                        ))}
+                           ))}
 
-                        {/* <div className="orderStatus">
+                           {/* <div className="orderStatus">
                            <div className="point"></div>
                            <div className="orderInfo">
                               <div className="status">Packed</div>
@@ -124,44 +126,35 @@ const Orders = (props) => {
                               <div className="date">Fri, 2020</div>
                            </div>
                         </div> */}
-                     </div>
+                        </div>
 
-                     {/* select input to apply order action */}
-                     <div
-                        style={{
-                           padding: "0 50px",
-                           boxSizing: "border-box",
-                        }}
-                     >
-                        <select onChange={(e) => setType(e.target.value)}>
-                           <option value={""}>select status</option>
-                           {orderItem.orderStatus.map((status) => {
-                              return (
-                                 <>
-                                    {!status.isCompleted ? (
-                                       <option
-                                          key={status.type}
-                                          value={status.type}
-                                       >
-                                          {status.type}
-                                       </option>
-                                    ) : null}
-                                 </>
-                              );
-                           })}
-                        </select>
-                     </div>
-                     {/* button to confirm action */}
+                        {/* select input to apply order action */}
+                        <div className="order-track-button">
+                           <select onChange={(e) => setType(e.target.value)}>
+                              <option value={""}>select status</option>
+                              {orderItem.orderStatus.map((status) => {
+                                 return (
+                                    <>
+                                       {!status.isCompleted ? (
+                                          <option
+                                             key={status.type}
+                                             value={status.type}
+                                          >
+                                             {status.type}
+                                          </option>
+                                       ) : null}
+                                    </>
+                                 );
+                              })}
+                           </select>
+                        </div>
+                        {/* button to confirm action */}
 
-                     <div
-                        style={{
-                           padding: "0 50px",
-                           boxSizing: "border-box",
-                        }}
-                     >
-                        <button onClick={() => onOrderUpdate(orderItem._id)}>
-                           confirm
-                        </button>
+                        <div className="order-track-button">
+                           <button onClick={() => onOrderUpdate(orderItem._id)}>
+                              confirm
+                           </button>
+                        </div>
                      </div>
                   </div>
                </Card>
