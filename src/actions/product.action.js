234a0bc9c6bfcb929f0321d4252d48d483d89bde
Elian Gonzalez <eliancarlogm@gmail.com>
2023-10-18 23:37:56 -0400

    test(src/actions/product.action.js): prove the product data

--- src/actions/product.action.js
+++ src/actions/product.action.js
@@ -1,4 +1,5 @@
 import axios from "../helpers/axios.js";
+import { API_IMBGG_UPLOAD } from "../urlConfig.js";
 import { productConstants } from "./constants.js";
 
 const getProducts = () => {
@@ -25,6 +26,7 @@ export const addProduct = (form) => {
    return async (dispatch) => {
       try {
          dispatch({ type: productConstants.ADD_PRODUCT_REQUEST });
+
          const res = await axios.post(`product/create`, form);
          if (res.status === 201) {
             dispatch({ type: productConstants.ADD_PRODUCT_SUCCESS });
