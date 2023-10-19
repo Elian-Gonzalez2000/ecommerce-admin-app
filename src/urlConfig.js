aa85c2940b7b0ce9978fc8a861a186ffc36265df
Elian Gonzalez <eliancarlogm@gmail.com>
2023-10-18 23:39:40 -0400

    chores: prettier format changes

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -2,3 +2,5 @@ export const api = "http://localhost:3002/api";
 export const genericPublicUrl = (fileName) => {
    return `http://localhost:3002/public/${fileName}`;
 };
+export const API_IMBGG_UPLOAD =
+   "https://api.imgbb.com/1/upload?key=ee6acd04b5e320009ed4c54b3c5070b8";
